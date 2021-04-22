69b583411215a00b777df9e3c733dab5643269a2
Aarón Alfonseca <Aarn Alfonseca@alfonzka_13>
2021-04-22 17:39:45 -0500

    Nuevo Tema "Hilos" y "Deadlock"

--- src/Hilos/SimpleThreads.java
+++ src/Hilos/SimpleThreads.java
@@ -6,8 +6,7 @@ package Hilos;
  */
 public class SimpleThreads {
 
-    // Display a message, preceded by
-    // the name of the current thread
+    // Se muestra un mensaje, precediendo por el nombre del hilo actual 
     static void threadMessage(String message) {
         String threadName
                 = Thread.currentThread().getName();
@@ -30,9 +29,9 @@ public class SimpleThreads {
                 for (int i = 0;
                         i < importantInfo.length;
                         i++) {
-                    // Pause for 4 seconds
+                    // Descanso por 4 seg
                     Thread.sleep(4000);
-                    // Print a message
+                    // Impresion en consola
                     threadMessage(importantInfo[i]);
                 }
             } catch (InterruptedException e) {
@@ -44,14 +43,10 @@ public class SimpleThreads {
     public static void main(String args[])
             throws InterruptedException {
 
-        // Delay, in milliseconds before
-        // we interrupt MessageLoop
-        // thread (default one hour).
+        //Aqui se hace un retraso, en milisegundos antes de que se interrumpa el hilo MessageLoop (predeterminado una hora). 
         long patience = 1000 * 60 * 60;
 
-        // If command line argument
-        // present, gives patience
-        // in seconds.
+        // Si el argumento de la línea de comando está presente, queda esperar los segundos. 
         if (args.length > 0) {
             try {
                 patience = Long.parseLong(args[0]) * 1000;
